42430a896da4481961f2811e80920b641bda29ee
p5na9 <sp5na92@gmail.com>
2022-04-20 16:41:14 +0900

    c

---
+++ eslint-test/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        //eslint 홈페이지
+        "semi": ["error", "always"],
+    }
+}
